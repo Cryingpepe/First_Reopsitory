b5b162ce722261c42a956a243e3762131b7df9b1
Cryingpepe <82265937+Cryingpepe@users.noreply.github.com>
2021-05-29 20:25:40 +0900

    Add files via upload
    https://app.codility.com/programmers/lessons/9-maximum_slice_problem/max_profit/

---
+++ maxprofit.py
@@ -0,0 +1,18 @@
+# CSE 101
+# Lab 11
+# Name: Seongbeen Na
+# Email address: seongbeen.na@stonybrook.edu
+
+def solution(A):
+    result = []
+    for i in A[:-1]:
+        profit = max(A[A.index(i)+1:]) - i
+        if profit > 0:
+            result.append(profit)
+    
+    if len(result)==0:
+        return 0
+    
+    return max(result)
+
+print(solution( [23171, 21011, 21123, 21366, 21013, 21367])) # Test case should print 356
