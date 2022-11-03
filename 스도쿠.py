a5416be195da3970527cf7ba6ec51bccd5cfb64b
Cryingpepe <82265937+Cryingpepe@users.noreply.github.com>
2022-11-03 20:53:21 +0900

    Update 스도쿠.py

--- 스도쿠.py
+++ 스도쿠.py
@@ -1,1 +1,21 @@
+sudokuW = []
+sudokuH = [[],[],[],[],[],[],[],[],[]]
+for i in range(9):
+    sudokuW.append(input().split())
+
+for i in sudokuW:
+    for j in range(9):
+        if i.count("0") == 1:
+            i[i.index("0")] = str(j)
+
+for j in range(9):
+    for i in range(9):
+        sudokuH[i].append(sudokuW[j][i])
+
+for i in sudokuH:
+    for j in range(9):
+        if i.count("0") == 1:
+            i[i.index("0")] = str(j)
+
+print(sudokuH)
 
