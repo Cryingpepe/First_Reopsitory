c3d0d1099b268a641584dd62c7ef9d09afe6324d
Cryingpepe <82265937+Cryingpepe@users.noreply.github.com>
2022-11-08 11:25:06 +0900

    Update 스도쿠.py

--- 스도쿠.py
+++ 스도쿠.py
@@ -35,14 +35,16 @@ while end == 0:
         sudokuS[0]+=sudokuH[j][0:3]
         sudokuS[3]+=sudokuH[j][3:6]
         sudokuS[6]+=sudokuH[j][6:9]
+
         sudokuS[1]+=sudokuH[j+3][0:3]
         sudokuS[4]+=sudokuH[j+3][3:6]
         sudokuS[7]+=sudokuH[j+3][6:9]
+
         sudokuS[2]+=sudokuH[j+6][0:3]
         sudokuS[5]+=sudokuH[j+6][3:6]
         sudokuS[8]+=sudokuH[j+6][6:9]
     
-    for i in sudokuH:                     # S뺴기
+    for i in sudokuS:                     # S뺴기
         if i.count(0) == 1:
             i[i.index(0)] = 45 - sum(i)
 
@@ -51,17 +53,22 @@ while end == 0:
 
     sudokuH = [[],[],[],[],[],[],[],[],[]] # H리스트 초기화
 
-    for i in range(9):                     # W만들기 - 미완
-        for j in range(9):
-            sudokuW[i]+=sudokuS[::3][i::3]
-            sudokuW[i+3]+=sudokuS[1::3][i::3]
-            sudokuW[i+6]+=sudokuS[2::3][i::3]
+    for i in range(0,3):                     # W만들기 
+        sudokuW[i]+=sudokuS[0][i::3]
+        sudokuW[i]+=sudokuS[1][i::3]
+        sudokuW[i]+=sudokuS[2][i::3]
 
-result = ""
+        sudokuW[i+3]+=sudokuS[3][i::3]
+        sudokuW[i+3]+=sudokuS[4][i::3]
+        sudokuW[i+3]+=sudokuS[5][i::3]
+
+        sudokuW[i+6]+=sudokuS[6][i::3]
+        sudokuW[i+6]+=sudokuS[7][i::3]
+        sudokuW[i+6]+=sudokuS[8][i::3]
+
+
 
 for i in range(9):
     for j in range(9):
-        result += str(sudokuW[i][j]) + " "
-    result += "\n"
-
-print (result)
+        print (sudokuW[i][j], end=" ")
+    print ("\n")
