1d49a70211ed27a3c60034bcac14dace4f9345c2
Cryingpepe <82265937+Cryingpepe@users.noreply.github.com>
2022-11-11 23:14:22 +0900

    Update 평범한 배낭.py

--- 평범한 배낭.py
+++ 평범한 배낭.py
@@ -1,28 +1,25 @@
 #https://www.acmicpc.net/problem/12865          (https://www.acmicpc.net/workbook/view/4349)
 
+#https://velog.io/@na0ngkim/%EB%83%85%EC%83%89knapsack-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EB%B0%B0%EB%82%AD-%EB%AC%B8%EC%A0%9C-%EB%B0%B1%EC%A4%80-12865-%ED%8F%89%EB%B2%94%ED%95%9C-%EB%B0%B0%EB%82%AD-%ED%8C%8C%EC%9D%B4%EC%8D%AC
+
 Numofstf, maxweight = map(int,input().split())
-listofstf =[]
-currentstf = []
 
-currentw, currentv = 0, 0
+listofstf = []
+result = [[0 for i in range(maxweight+1)] for j in range(Numofstf+1)]
 
-while Numofstf > 0:
-    a, b = map(int,input().split())
-    listofstf.append([a,b])
-    Numofstf -= 1
 
-for i in listofstf:
-    if currentw + i[0] <= maxweight:
-        currentw += i[0]
-        currentv += i[1]
-        currentstf.append(i)
-    else:
-        for x in currentstf:
-            if (i[0] <= x[0] and i[1] > x[1]) or (i[1] == x[1] and i[0] < x[0]):
-                x[0], x[1] = i[0], i[1]
-            else:
-                break
+for i in range(Numofstf):
+    listofstf.append(list(map(int,input().split())))
 
-print(currentv)
-#본질적으로 잘못되어서 처음부터 바꿔야 할것 
+for i in range(1,Numofstf+1):
+    for j in range(1,maxweight+1):
+        if j < listofstf[i][0]:
+            result[i][j] = listofstf[i-1][1]
+        
+
+
+
+
+print (valueofstf)
+
 #찾아보니 냅색 알고리즘 이라고 한다
