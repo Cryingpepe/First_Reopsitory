a37e2f9b63029df2cfcd0e7c8b7ae674b15874aa
Cryingpepe <82265937+Cryingpepe@users.noreply.github.com>
2022-11-15 23:36:10 +0900

    Update 체스판 다시 칠하기.py

--- 체스판 다시 칠하기.py
+++ 체스판 다시 칠하기.py
@@ -1,1 +1,14 @@
+#https://www.acmicpc.net/problem/1018
+y, x = map(int ,input().split())
 
+map = []
+
+for i in range(y):
+    map.append(input())
+
+for i in range(0,x-7):
+    for j in range(0,y-7):
+        map[i][j:j+7].count("W")
+        map[i][j:j+7]
+
+print(map)
