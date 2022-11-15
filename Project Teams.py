329e4553deee8cded98fec38fcd53d4d12b11950
Cryingpepe <82265937+Cryingpepe@users.noreply.github.com>
2022-11-15 22:39:29 +0900

    Update Project Teams.py

--- Project Teams.py
+++ Project Teams.py
@@ -1,1 +1,13 @@
+#https://www.acmicpc.net/problem/20044
+NofT = int(input())
 
+listofmember = []
+listofmember += list(map(int, input().split()))
+listofmember.sort()
+
+result = []
+
+for x in range(NofT):
+    result.append(listofmember.pop()+listofmember.pop(0))
+
+print (min(result))
