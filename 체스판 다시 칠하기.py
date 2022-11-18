8ed4b7d4a2d5fe818be8217fdfd04ceab1b480fa
Cryingpepe <82265937+Cryingpepe@users.noreply.github.com>
2022-11-18 23:44:31 +0900

    Update 체스판 다시 칠하기.py

--- 체스판 다시 칠하기.py
+++ 체스판 다시 칠하기.py
@@ -8,35 +8,42 @@ for i in range(y):
 
 result = [] #[[0,0] for q in range((y-7)*(x-7))]
 
-ylist = [i for i in range(0,y)]
-xlist = [i for i in range(0,x)]
+ylist = list(range(0,y))
+xlist = list(range(0,x))
+
+# for w in xlist[:x-7]:
+#     for q in ylist[:y-7]:
+#         a = 0
+#         for i in range(0,8):
+#             a += map[i+q][0+w:8+w].count("W")
+#         result.append([a])
+
+isitodd = list(range(1,8,2))
 
 for w in xlist[:x-7]:
     for q in ylist[:y-7]:
         a = 0
-        for i in range(0,8):
-            a += map[i+q][0+w:8+w].count("W")
-        result.append([a])
-
-for w in xlist[:x-7]:
-    for q in ylist[:y-7]:
         b = 0
         for i in range(0,8):
-            if max(result)[0] > 32:
-                if map[i+q][0] == 'W':
-                    b += map[i+q][0+w:8+w:2].count("W")
-                    b += map[i+q][1+w:8+w:2].count("B")
-                else:
-                    b += map[i+q][0+w:8+w:2].count("B")
-                    b += map[i+q][1+w:8+w:2].count("W")
-            elif max(result)[0] < 32:
-                if map[i+q][0] == 'W':
-                    b += map[i+q][0+w:8+w:2].count("B")
-                    b += map[i+q][1+w:8+w:2].count("W")
+            words = ['W','B']
+            firstword = map[i+q][0+w]
+            words.pop(words.index(firstword))
+            secondword = words[0]
+            if i in isitodd:
+                a += map[i+q][0+w:8+w:2].count(secondword)
+                a += map[i+q][1+w:8+w:2].count(firstword)
+                b += map[i+q][0+w:8+w:2].count(firstword)
+                b += map[i+q][1+w:8+w:2].count(secondword)
+            else:
+                a += map[i+q][0+w:8+w:2].count(firstword)
+                a += map[i+q][1+w:8+w:2].count(secondword)
+                b += map[i+q][0+w:8+w:2].count(secondword)
+                b += map[i+q][1+w:8+w:2].count(firstword)
+                
 
-
-
+        print(a,b)
+        result.append(min(a,b))
 
 print(map)
 print(result)
-print(abs(max(result)[0]-32))
+print(min(result))
