fc561a616f2479bba9aa0e925068ff38a287dad4
Cryingpepe <82265937+Cryingpepe@users.noreply.github.com>
2023-01-17 13:44:34 +0900

    Update 북극곰은 괄호를 찢어(failed).py

--- 북극곰은 괄호를 찢어(failed).py
+++ 북극곰은 괄호를 찢어(failed).py
@@ -1,41 +1,42 @@
-import sys
-sys.setrecursionlimit(10**6)
-
 N = int(input())
 x = input()
-counts = 0  
     
 def solver(ps):
-    global counts
-    
-    if len(ps) == 0:
-        print (counts)
-        exit()
-    
-    listofps = [x for x in ps.split('()')]
 
-    rest = ''
-    
-    for i in listofps:
-        rest += i
-        
-    listofps = [x for x in rest.split(')(')]
+    stack = 0
+    maxstack = 0
 
-    rest = ''
-    
-    for i in listofps:
-        rest += i
-        
-    counts += 1
-    
-    solver(rest)
-    
+
+    for i in ps:
+
+        if i == '(':
+            stack += 1
+
+            if stack > maxstack:
+                maxstack = stack
+
+        else:
+            stack -= 1
+
+    stack = 0
+
+
+    for i in ps:
+
+        if i == ')':
+            stack += 1
+
+            if stack > maxstack:
+                maxstack = stack
+
+        else:
+            stack -= 1
+
+    print(maxstack)
+
 if N % 2 == 1:
     print(-1)      
 elif x.count('(') != x.count(')'):
     print(-1)      
 else:
-    solver (x)
-
-    
-    #시간초과.. 어떻게 최적화 시킬수 
+    solver(x)
