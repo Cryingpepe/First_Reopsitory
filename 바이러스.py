bb1350be3759b0eac471d59f3756f57d7e14e822
Cryingpepe <82265937+Cryingpepe@users.noreply.github.com>
2023-01-17 14:20:39 +0900

    Update 바이러스.py

--- 바이러스.py
+++ 바이러스.py
@@ -1,1 +1,12 @@
+N = int(input()) # Number of computer
+NumofCon = int(input()) # Number of connections
 
+ListofCon = [] # List of connections
+ListofVic = [] # List of Victim
+
+for i in range(NumofCon):
+    ListofCon += input().split()
+
+print (.index('1'))
+
+print (ListofCon)
