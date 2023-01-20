fc41951884e322b85cd8b5072b2ccf295d2983fe
Cryingpepe <82265937+Cryingpepe@users.noreply.github.com>
2023-01-21 04:29:12 +0900

    Update 바이러스.py

--- 바이러스.py
+++ 바이러스.py
@@ -1,11 +1,31 @@
 N = int(input()) # Number of computer
 NumofCon = int(input()) # Number of connections
 
-ListofCon = [] # List of connections
-ListofVic = [1] # List of Victim
+ListofVic = ['1'] # List of Victim
+ListofUninf = [] # List of Uninfected
 
 for i in range(NumofCon):
-    if input().split() in [a for a in ListofVic] :
-        ListofVic += input().split()
 
-print (ListofVic)
+    a,b = input().split()
+
+    if a in [x for x in ListofVic]:
+        if b in ListofVic:
+            continue
+        ListofVic += b
+
+    elif b in [x for x in ListofVic]:
+        if a in ListofVic:
+            continue
+        ListofVic += a
+
+    else:
+        ListofUninf.append([str(a),str(b)])
+
+for i in ListofVic:
+    for x in ListofUninf:
+        if i == x[0]:
+            ListofVic += x[1]
+        elif i == x[1]:
+            ListofVic += x[0]
+
+print(len(ListofVic)-1)
