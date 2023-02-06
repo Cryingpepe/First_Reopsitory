72cac3701d2a28ee6c50b269d8d8cf43cf86ab44
Cryingpepe <82265937+Cryingpepe@users.noreply.github.com>
2023-02-06 22:30:40 +0900

    Update 통계학.py

--- 통계학.py
+++ 통계학.py
@@ -1,10 +1,11 @@
+import sys
 listofn = []
 howmanyns = {}
 
 n = int(input())
 
 for i in range(n):
-    a = int(input())
+    a = int(sys.stdin.readline())
     listofn.append(a)
     if str(a) in howmanyns.keys():
         howmanyns[str(a)] += 1
@@ -13,14 +14,26 @@ for i in range(n):
 
 listofn.sort()
 
-print(int(sum(listofn) / len(listofn)))
+print(round(sum(listofn) / len(listofn)))
 
 print(listofn[int(len(listofn)/2)])
 
-# if list(howmanyns.values()).count((list(howmanyns.values())).index(max(list(howmanyns.values())))) > 1:
-#     print()
-# else:
-print(list(howmanyns.keys())[(list(howmanyns.values())).index(max(list(howmanyns.values())))])
+numofmax = list(howmanyns.values()).count(max(list(howmanyns.values())))
 
-print(abs(max(listofn) - min(listofn)))
+listofmax = []
 
+if numofmax == 1:
+    print(list(howmanyns.keys())[(list(howmanyns.values())).index(max(list(howmanyns.values())))])
+else:
+    for key, value in howmanyns.items():
+        if value == max(list(howmanyns.values())):
+            listofmax.append(key)
+
+    result = []
+    for i in listofmax:
+        result.append(int(i))
+
+    result.sort()
+    print(result[1])
+
+print(abs(listofn[-1] - (listofn[0])))
