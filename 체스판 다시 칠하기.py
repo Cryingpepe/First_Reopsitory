d44fff266225c00db334e32cb1a53ec238f813d6
Cryingpepe <82265937+Cryingpepe@users.noreply.github.com>
2023-07-11 14:32:34 +0900

    Update 체스판 다시 칠하기.py

--- 체스판 다시 칠하기.py
+++ 체스판 다시 칠하기.py
@@ -11,13 +11,6 @@ result = [] #[[0,0] for q in range((y-7)*(x-7))]
 ylist = list(range(0,y))
 xlist = list(range(0,x))
 
-# for w in xlist[:x-7]:
-#     for q in ylist[:y-7]:
-#         a = 0
-#         for i in range(0,8):
-#             a += map[i+q][0+w:8+w].count("W")
-#         result.append([a])
-
 isitodd = list(range(1,8,2))
 
 for w in xlist[:x-7]:
@@ -35,15 +28,17 @@ for w in xlist[:x-7]:
                 b += map[i+q][0+w:8+w:2].count(firstword)
                 b += map[i+q][1+w:8+w:2].count(secondword)
             else:
-                a += map[i+q][0+w:8+w:2].count(firstword)
-                a += map[i+q][1+w:8+w:2].count(secondword)
-                b += map[i+q][0+w:8+w:2].count(secondword)
-                b += map[i+q][1+w:8+w:2].count(firstword)
-                
+                b += map[i+q][0+w:8+w:2].count(firstword)
+                b += map[i+q][1+w:8+w:2].count(secondword)
+                a += map[i+q][0+w:8+w:2].count(secondword)
+                a += map[i+q][1+w:8+w:2].count(firstword)
+             
 
-        print(a,b)
+        # print(a,b)
         result.append(min(a,b))
 
-print(map)
-print(result)
+# print(map)
+# print(result)
 print(min(result))
+
+# 예제 2번 빼고 다 맞는데 뭐가 문제일까
